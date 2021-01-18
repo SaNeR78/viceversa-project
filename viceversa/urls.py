6320aabbbce36422cbc2d0508f69402d9fb09408
Aleksandr <avssasha@gmail.com>
2021-01-18 19:34:24 +0300

    AddResultAndButtonBack

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+	path('', views.home, name='home'),
+    path('reverse/', views.reverse, name='reverse'),
 ]
