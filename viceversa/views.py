aa7059d866884aaff053e84229d628db864a509a
Aleksandr <avssasha@gmail.com>
2021-01-18 19:54:14 +0300

    CountWords

--- viceversa/views.py
+++ viceversa/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 def home(request):
 	return render(request, 'home.html')
 
+def countWords(a):
+	word_list = a.split()
+	return len(word_list)
+
 def reverse(request):
 	get_text = request.GET['usertext']
 	reversed_text = get_text[::-1]
-	return render(request, 'reverse.html', {'usertext':reversed_text})
+	count_words = countWords(reversed_text)
+	return render(request, 'reverse.html', {'usertext':reversed_text,
+		'resulting':count_words})
